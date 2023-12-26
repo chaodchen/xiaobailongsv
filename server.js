2da94ef47d1249a89ff0a75fc91697a7a9ec5b8b
chaodchen <17685034710@163.com>
2023-12-26 19:29:25 +0800

    添加req.headers

--- server.js
+++ server.js
@@ -170,6 +170,7 @@ function main () {
             });
             res.end('Pong', 'utf-8');
         } else if (url == '/') {
+            console.log(req.headers);
             res.writeHead(200, {
                 "Content-Type": "text/plain; charset=utf-8",
             });
